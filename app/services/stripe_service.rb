fec8899aa0779733e7a6b2882240ac2e1053e707
askumardev <askumar.dev@outlook.com>
2022-07-19 21:52:50 +0530

    strp service to create customer added

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -3,4 +3,18 @@ class StripeService
   def initialize()
     Stripe.api_key = ENV['STRIPE_SECRET_KEY']
   end
+
+  def find_or_create_customer(customer)
+    if customer.stripe_customer_id.present?
+      stripe_customer = Stripe::Customer.retrieve(customer.stripe_customer_id)
+    else
+      stripe_customer = Stripe::Customer.create({
+        name: customer.full_name, 
+        email: customer.email, 
+        phone: customer.contact_number
+      })
+      customer.update(stripe_customer_id: stripe_customer.id)
+      stripe_customer
+    end
+  end
 end
