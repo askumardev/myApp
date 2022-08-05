42538350fa3a954e07ef5f5820a047c53e322110
askumardev <askumar.dev@outlook.com>
2022-08-05 20:08:07 +0530

    scraper controller code added

--- app/controllers/scraper_controller.rb
+++ app/controllers/scraper_controller.rb
@@ -1,4 +1,9 @@
 class ScraperController < ApplicationController
   def index
+    site = "https://www.bbc.co.uk/"
+    allowed_site = 'bbc.co'
+    page = Nokogiri::HTML(URI.open(site))
+    links = page.xpath('//a')
+    @links = links.map{ |element| element["href"] if element["href"].include? allowed_site}.compact
   end
 end
